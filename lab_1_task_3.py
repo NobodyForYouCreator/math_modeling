58b5fb42b01a7ee39a5acfd5697090f4196516ac
NobodyForYou <denis@krukover.ru>
2022-12-26 10:41:42 +0200

    backup

--- lab_1_task_3.py
+++ lab_1_task_3.py
@@ -2,8 +2,7 @@ from math import sqrt
 
 
 def first_opr(x):
-  return (sqrt(x**3) / (x**3 + 3 / x)) * (
-    4 * x**7 - x**5) + 80 * sqrt(27 * x**4 + 12 * x**3 - 5 * x**2 + 10)
+  return (sqrt(x**3) / (x**3 + 3 / x)) * (4 * x**7 - x**5) + 80 * sqrt(27 * x**4 + 12 * x**3 - 5 * x**2 + 10)
 
 
 def second_opr(x):
